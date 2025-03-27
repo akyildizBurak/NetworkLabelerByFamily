3eb185377b4667e09b821989ff1d249ba4ddf55a
Burak AKYILDIZ <akyildiz.burak@gmai.com>
2025-03-27 15:22:52 +0300

    Some additional features like being able to chose current style to modify, being able to delete exiting style etc.

---
+++ PartFamilyData.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+
+namespace PartFamilyData
+{
+    public class PartFamilyData : INotifyPropertyChanged
+    {
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+} 
